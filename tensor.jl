c320844f7ca88f383f9ab30ffce1bcce8234bc1d
Felix Kunzmann <fe.kunzmann@gmail.com>
2021-06-16 15:02:42 +0200

    Rename Sector to SymmetrySector

--- tensor.jl
+++ tensor.jl
@@ -2,22 +2,22 @@ module Tensors
 
 import Base.ImmutableDict
 
-abstract type Sector end
+abstract type SymmetrySector end
 
 # TODO: macro to create Sector types
 
-struct Space{S <: Sector}
+struct Space{S <: SymmetrySector}
     dimensions::ImmutableDict{S, Int}
     dual::Bool
 end
 
-struct Connection{S <: Sector}
+struct Connection{S <: SymmetrySector}
     name::Symbol
     space::Space{S}
     tags::ImmutableDict{Symbol, Int}
 end
 
-struct Tensor{T <: Number, S <: Sector, N} 
+struct Tensor{T <: Number, S <: SymmetrySector, N} 
     components::Dict{NTuple{N, S}, Array{T, N}}
     connections::NTuple{N, Connection{S}}
 end
