62198c82a3db5c0aaae4fd6ace5adb196b18a7ac
Felix Kunzmann <fe.kunzmann@gmail.com>
2021-07-26 10:34:32 +0200

    Improve and move code for SymmetrySectors.

--- src/BlockTensors.jl
+++ src/BlockTensors.jl
@@ -8,74 +8,8 @@ export SymmetrySector, Trivial, @SymmetrySector
 export Connection, Connector, replicate, dual, matchconnectors
 export Tensor, mergeconnectors, separateconnectors, svd, qr, lq
 
-abstract type SymmetrySector end
-
-struct Trivial <: SymmetrySector end
-
-macro SymmetrySector(name::Symbol, numbers)
-    # adapted from @NamedTuple
-    name = esc(name)
-    Meta.isexpr(numbers, [:braces, :block]) || throw(
-        ArgumentError("@Sector expects {...} or begin ... end as second argument")
-    )
-    decls = filter(e -> !(e isa LineNumberNode), numbers.args)
-    all(
-        e -> e isa Symbol || 
-            (Meta.isexpr(e, :(::)) && e.args[1] isa Symbol && eval(e.args[2]) <: Integer),
-        decls
-    ) || throw(
-        ArgumentError(
-            "second argument of @Sector must be a sequence of name[::type] expressions"
-            * " with type <: Integer"
-        )
-    )
-    fields = Symbol[e isa Symbol ? e : e.args[1] for e in decls]
-    types = [esc(e isa Symbol ? Int : e.args[2]) for e in decls]
-    numbers = (:($field::$type) for (field, type) in zip(fields, types))
-    args = (Expr(:kw, :($(esc(field))::Integer), 0) for field in fields)
-    conversions = (
-        :(convert($type, $(esc(field)))) for (field, type) in zip(fields, types)
-    )
-    constructor = :($name(; $(args...)) = new($(conversions...)))
-    return Expr(
-        :struct, false,                         # declare an immutable struct
-        :($name <: SymmetrySector),
-        Expr(:block, numbers..., constructor)
-    )
-end
-
-function Base.isless(x::S, y::S) where S <: SymmetrySector
-    for field in fieldnames(S)
-        getfield(x, field) < getfield(y, field) || return false
-    end
-    return true
-end
-
-function Base.:+(x::S, ys::S...) where S <: SymmetrySector
-    S(; 
-        (
-            field => +(getfield(x, field), map(y -> getfield(y, field), ys)...) 
-            for field in fieldnames(S)
-        )...
-    )
-end
-
-function Base.:-(x::S) where S <: SymmetrySector
-    S(; (field => -(getfield(x, field)) for field in fieldnames(S))...)
-end
-
-function Base.show(io::IO, x::S) where S <: SymmetrySector
-    show(io, S)
-    print(io, "(")
-    sep = ""
-    for field in fieldnames(S)
-        print(io, sep, field, " = ")
-        show(io, getfield(x, field))
-        sep = ", "
-    end
-    print(io, ")")
-end
-
+include("util.jl")
+include("symmetry.jl")
 
 SymbolOrString = Union{Symbol, AbstractString}
 
---
+++ src/symmetry.jl
@@ -0,0 +1,65 @@
+abstract type SymmetrySector end
+
+struct Trivial <: SymmetrySector end
+
+macro SymmetrySector(name::Symbol, numbers)
+    # adapted from @NamedTuple
+    name = esc(name)
+    Meta.isexpr(numbers, [:braces, :block]) || throw(
+        ArgumentError("@Sector expects {...} or begin ... end as second argument")
+    )
+    decls = filter(e -> !(e isa LineNumberNode), numbers.args)
+    all(
+        e -> e isa Symbol || 
+            (Meta.isexpr(e, :(::)) && e.args[1] isa Symbol && eval(e.args[2]) <: Integer),
+        decls
+    ) || throw(
+        ArgumentError(
+            "second argument of @Sector must be a sequence of name[::type] expressions"
+            * " with type <: Integer"
+        )
+    )
+    fields = Symbol[e isa Symbol ? e : e.args[1] for e in decls]
+    types = [esc(e isa Symbol ? Int : e.args[2]) for e in decls]
+    numbers = (:($field::$type) for (field, type) in zip(fields, types))
+    args = (Expr(:kw, :($(esc(field))::Integer), 0) for field in fields)
+    conversions = (
+        :(convert($type, $(esc(field)))) for (field, type) in zip(fields, types)
+    )
+    constructor = :($name(; $(args...)) = new($(conversions...)))
+    return Expr(
+        :struct, false,                         # declare an immutable struct
+        :($name <: SymmetrySector),
+        Expr(:block, numbers..., constructor)
+    )
+end
+
+function Base.isless(x::S, y::S) where S <: SymmetrySector
+    all(fieldnames(S)) do field
+        getfield(x, field) < getfield(y, field)
+    end
+end
+
+function Base.:+(x::S, ys::S...) where S <: SymmetrySector
+    S(; 
+        (
+            field => +(getfield(x, field), getfield.(ys, field)...) 
+            for field in fieldnames(S)
+        )...
+    )
+end
+
+function Base.:-(x::S) where S <: SymmetrySector
+    S(; (field => -(getfield(x, field)) for field in fieldnames(S))...)
+end
+
+function Base.show(io::IO, x::S) where S <: SymmetrySector
+    compact = get(io, :compact, false)
+    show(io, S)
+    print(io, "(")
+    join(io, fieldnames(S)) do io, field
+        print(io, field, compact ? "=" : " = ")
+        show(io, getfield(x, field))
+    end
+    print(io, ")")
+end
